18113ffc7e99796f048788f8ead47ec235c559be
Arunava2006webdev <bbitarunava2006@gmail.com>
2026-07-20 23:25:08 +0530

    Updated database connection for Aiven

--- Backend/db.js
+++ Backend/db.js
@@ -1,36 +1,23 @@
 const mysql = require("mysql2");
-require("dotenv").config();
 
 const connection = mysql.createConnection({
-
     host: process.env.DB_HOST,
-
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
-
     password: process.env.DB_PASSWORD,
-
     database: process.env.DB_NAME,
-
-    port: process.env.DB_PORT
-
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
-connection.connect((error)=>{
-
-    if(error){
-
+connection.connect((err) => {
+    if (err) {
         console.log("Database Connection Failed");
-
-        console.log(error);
-
-    }
-
-    else{
-
+        console.log(err);
+    } else {
         console.log("MySQL Connected");
-
     }
-
 });
 
 module.exports = connection;
